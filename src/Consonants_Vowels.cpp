c4c3c967aa9e5a07c73c9f29ddb8827a360d7f18
tech1arb <tech1arb@missionrnd.com>
2016-01-29 06:37:31 +0530

    Submit Commit

--- src/Consonants_Vowels.cpp
+++ src/Consonants_Vowels.cpp
@@ -20,7 +20,27 @@ NOTES: Don't create new string , Dont return anything ,you have been given two p
 #include <stddef.h>
 
 
-void count_vowels_and_consonants(char *str,int *consonants, int *vowels){
-
-
-}
+void count_vowels_and_consonants(char *str, int *consonants, int *vowels)
+{
+	if (str == NULL)
+	{
+		*consonants = 0;
+		*vowels = 0;
+	}
+	else
+	{
+		int i = 0;
+		int count1 = 0;
+		int count2 = 0;
+		for (i = 0; i < str[i] != '\0'; i++)
+		{
+			if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+				count1++;
+			else if ((str[i] >= 'a'&& str[i] <= 'z') || (str[i] >= 'A'&& str[i] <= 'Z'))
+				count2++;
+		}
+		*consonants = count2;
+		*vowels = count1;
+		//printf("count1=%d count2=%d", count1, count2);
+	}
+	}
